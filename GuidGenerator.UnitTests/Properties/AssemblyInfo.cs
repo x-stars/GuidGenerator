dfbf8deb44bf3b1c2601e5bc2cfeac918842998a
天南十字星 <zjh96@live.cn>
2022-03-05 22:06:41 +0800

    Add polyfill and assembly info sources.

---
+++ GuidGenerator.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System;
+
+// 指示程序元素是否使用公共语言规范进行编译。
+[assembly: CLSCompliant(true)]
---
+++ GuidGenerator.UnitTests/Properties/LanguageSupport.cs
@@ -0,0 +1,50 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#if !NET5_0_OR_GREATER
+namespace System.Runtime.CompilerServices
+{
+    using System.ComponentModel;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [CompilerGenerated, DebuggerNonUserCode]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit
+    {
+    }
+}
+#endif
+
+#if !NETCOREAPP3_0_OR_GREATER
+namespace System.Runtime.CompilerServices
+{
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Allows capturing of the expressions passed to a method.
+    /// </summary>
+    [CompilerGenerated, DebuggerNonUserCode]
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    internal sealed class CallerArgumentExpressionAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CallerArgumentExpressionAttribute"/> class.
+        /// </summary>
+        /// <param name="parameterName">The name of the targeted parameter.</param>
+        public CallerArgumentExpressionAttribute(string parameterName)
+        {
+            this.ParameterName = parameterName;
+        }
+
+        /// <summary>
+        /// Gets the target parameter name of the CallerArgumentExpression.
+        /// </summary>
+        /// <returns>The name of the targeted parameter of the CallerArgumentExpression.</returns>
+        public string ParameterName { get; }
+    }
+}
+#endif
