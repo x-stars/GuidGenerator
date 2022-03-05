7ca9d0c5ed9138f46313ad6802d3eebb8f978c93
天南十字星 <zjh96@live.cn>
2022-03-05 22:05:42 +0800

    Add unit test and console app projects.

---
+++ GuidGenerator.UnitTests/GuidGeneratorTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XstarS.GuidGenerators
+{
+    [TestClass]
+    public class GuidGeneratorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
