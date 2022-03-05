665dc53acf6625e89af677b0c9a9fc4b901e62db
天南十字星 <zjh96@live.cn>
2022-03-06 01:32:29 +0800

    Implement basic functions of console app.

--- GuidGenerator.CommandLine/Program.cs
+++ GuidGenerator.CommandLine/Program.cs
@@ -1,3 +1,17 @@
 ﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace XstarS.GuidGenerators
+{
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            foreach (var verNum in new[] { 1, 3, 4, 5 })
+            {
+                var version = (GuidVersion)verNum;
+                var guid = GuidGenerator.NewGuid(version);
+                Console.WriteLine(guid.ToString());
+            }
+        }
+    }
+}
