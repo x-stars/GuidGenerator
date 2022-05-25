25af14e703e3280ce1d938a6aa9b5e9906d7e4a2
天南十字星 <zjh96@live.cn>
2022-05-25 22:43:45 +0800

    Add GUID generation benchmark project.

---
+++ GuidGenerator.Benchmarks/BenchmarkProgram.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace XstarS.GuidGenerators
+{
+    internal static class BenchmarkProgram
+    {
+        internal static void Main(string[] args)
+        {
+            var assembly = typeof(BenchmarkProgram).Assembly;
+            var summaries = BenchmarkRunner.Run(assembly);
+        }
+    }
+}
---
+++ GuidGenerator.Benchmarks/GuidGeneratorBenchmark.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace XstarS.GuidGenerators
+{
+    using static GuidVersion;
+
+    public class GuidGeneratorBenchmark
+    {
+        [CLSCompliant(false)]
+        [Params(1, 10, 100, 1000)]
+        public int GuidCount;
+
+        [Benchmark(Baseline = true)]
+        public void GuidNewGuid()
+        {
+            var count = this.GuidCount;
+            for (int index = 0; index < count; index++)
+            {
+                var guid = Guid.NewGuid();
+            }
+        }
+
+        [Benchmark]
+        public void EmptyGenerate()
+        {
+            var guidGen = GuidGenerator.OfVersion(Empty);
+            var count = this.GuidCount;
+            for (int index = 0; index < count; index++)
+            {
+                var guid = guidGen.NewGuid();
+            }
+        }
+
+        [Benchmark]
+        public void GuidV1Generate()
+        {
+            var guidGen = GuidGenerator.OfVersion(Version1);
+            var count = this.GuidCount;
+            for (int index = 0; index < count; index++)
+            {
+                var guid = guidGen.NewGuid();
+            }
+        }
+
+        [Benchmark]
+        public void GuidV2Generate()
+        {
+            var guidGen = GuidGenerator.OfVersion(Version2);
+            var count = this.GuidCount;
+            for (int index = 0; index < count; index++)
+            {
+                var guid = guidGen.NewGuid();
+            }
+        }
+
+        [Benchmark]
+        public void GuidV3Generate()
+        {
+            var guidGen = GuidGenerator.OfVersion(Version3);
+            var count = this.GuidCount;
+            for (int index = 0; index < count; index++)
+            {
+                var guid = guidGen.NewGuid();
+            }
+        }
+
+        [Benchmark]
+        public void GuidV4Generate()
+        {
+            var guidGen = GuidGenerator.OfVersion(Version4);
+            var count = this.GuidCount;
+            for (int index = 0; index < count; index++)
+            {
+                var guid = guidGen.NewGuid();
+            }
+        }
+
+        [Benchmark]
+        public void GuidV5Generate()
+        {
+            var guidGen = GuidGenerator.OfVersion(Version5);
+            var count = this.GuidCount;
+            for (int index = 0; index < count; index++)
+            {
+                var guid = guidGen.NewGuid();
+            }
+        }
+    }
+}
