c4ef8021ce0b09295a2671bda9547d7538b2dca4
天南十字星 <zjh96@live.cn>
2022-06-01 21:31:41 +0800

    Add version-specified GUID generate methods.

---
+++ XstarS.GuidGenerators/GuidGenerator.NewGuid.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace XstarS.GuidGenerators
+{
+    partial class GuidGenerator
+    {
+        /// <summary>
+        /// 生成一个指定版本的 <see cref="Guid"/> 实例。
+        /// </summary>
+        /// <param name="version">要生成的 <see cref="Guid"/> 的版本。</param>
+        /// <returns>一个版本为 <paramref name="version"/> 的 <see cref="Guid"/> 实例。</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="version"/> 不为有效的 <see cref="GuidVersion"/> 枚举值。</exception>
+        public static Guid NewGuid(GuidVersion version)
+        {
+            return GuidGenerator.OfVersion(version).NewGuid();
+        }
+
+        /// <summary>
+        /// 根据指定的命名空间和名称生成一个指定版本的 <see cref="Guid"/> 实例。
+        /// </summary>
+        /// <param name="version">要生成的 <see cref="Guid"/> 的版本。</param>
+        /// <param name="ns">生成 <see cref="Guid"/> 时使用的命名空间。</param>
+        /// <param name="name">生成 <see cref="Guid"/> 时使用的名称。</param>
+        /// <returns>根据 <paramref name="ns"/> 和 <paramref name="name"/>
+        /// 生成的版本为 <paramref name="version"/> 的 <see cref="Guid"/> 实例。</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="version"/> 不为有效的 <see cref="GuidVersion"/> 枚举值。</exception>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="name"/> 为 <see langword="null"/>。</exception>
+        public static Guid NewGuid(GuidVersion version, Guid ns, string name)
+        {
+            return GuidGenerator.OfVersion(version).NewGuid(ns, name);
+        }
+
+        /// <summary>
+        /// 生成一个 RFC 4122 UUID 版本 1 的 <see cref="Guid"/> 实例。
+        /// </summary>
+        /// <returns>一个 RFC 4122 UUID 版本 1 的 <see cref="Guid"/> 实例。</returns>
+        public static Guid NewGuidV1()
+        {
+            return GuidGenerator.NewGuid(GuidVersion.Version1);
+        }
+
+        /// <summary>
+        /// 根据指定的 DCE Security 域以及本地 ID 生成一个 RFC 4122 UUID 版本 2 的 <see cref="Guid"/> 实例。
+        /// </summary>
+        /// <param name="domain">生成 <see cref="Guid"/> 使用的 DCE Security 域。</param>
+        /// <param name="localID">自定义本地 ID 的 32 位整数。</param>
+        /// <returns>一个 RFC 4122 UUID 版本 2 的 <see cref="Guid"/> 实例。</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="domain"/> 不为有效的 <see cref="DceSecurityDomain"/> 枚举值。</exception>
+        public static Guid NewGuidV2(DceSecurityDomain domain, int? localID = null)
+        {
+            var guidGen = GuidGenerator.OfVersion(GuidVersion.Version2);
+            return guidGen.NewGuid(domain, localID);
+        }
+
+        /// <summary>
+        /// 根据指定的命名空间和名称生成一个 RFC 4122 UUID 版本 3 的 <see cref="Guid"/> 实例。
+        /// </summary>
+        /// <param name="ns">生成 <see cref="Guid"/> 时使用的命名空间。</param>
+        /// <param name="name">生成 <see cref="Guid"/> 时使用的名称。</param>
+        /// <returns>根据 <paramref name="ns"/> 和 <paramref name="name"/>
+        /// 生成的 RFC 4122 UUID 版本 3 的 <see cref="Guid"/> 实例。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="name"/> 为 <see langword="null"/>。</exception>
+        public static Guid NewGuidV3(Guid ns, string name)
+        {
+            return GuidGenerator.NewGuid(GuidVersion.Version3, ns, name);
+        }
+
+        /// <summary>
+        /// 生成一个 RFC 4122 UUID 版本 4 的 <see cref="Guid"/> 实例。
+        /// </summary>
+        /// <returns>一个 RFC 4122 UUID 版本 4 的 <see cref="Guid"/> 实例。</returns>
+        public static Guid NewGuidV4()
+        {
+            return GuidGenerator.NewGuid(GuidVersion.Version4);
+        }
+
+        /// <summary>
+        /// 根据指定的命名空间和名称生成一个 RFC 4122 UUID 版本 5 的 <see cref="Guid"/> 实例。
+        /// </summary>
+        /// <param name="ns">生成 <see cref="Guid"/> 时使用的命名空间。</param>
+        /// <param name="name">生成 <see cref="Guid"/> 时使用的名称。</param>
+        /// <returns>根据 <paramref name="ns"/> 和 <paramref name="name"/>
+        /// 生成的 RFC 4122 UUID 版本 5 的 <see cref="Guid"/> 实例。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="name"/> 为 <see langword="null"/>。</exception>
+        public static Guid NewGuidV5(Guid ns, string name)
+        {
+            return GuidGenerator.NewGuid(GuidVersion.Version5, ns, name);
+        }
+    }
+}
--- XstarS.GuidGenerators/GuidGenerator.cs
+++ XstarS.GuidGenerators/GuidGenerator.cs
@@ -5,62 +5,12 @@ namespace XstarS.GuidGenerators
     /// <summary>
     /// 提供生成 <see cref="Guid"/> 的方法。
     /// </summary>
-    public abstract class GuidGenerator
+    public abstract partial class GuidGenerator
     {
         /// <summary>
         /// 初始化 <see cref="GuidGenerator"/> 类的新实例。
         /// </summary>
         protected GuidGenerator() { }
-
-        /// <summary>
-        /// 获取指定 GUID 版本的 <see cref="GuidGenerator"/> 对象。
-        /// </summary>
-        /// <param name="version">要生成的 <see cref="Guid"/> 的版本。</param>
-        /// <returns>版本为 <paramref name="version"/> 的 <see cref="GuidGenerator"/>。</returns>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// <paramref name="version"/> 不为有效的 <see cref="GuidVersion"/> 枚举值。</exception>
-        public static GuidGenerator OfVersion(GuidVersion version)
-        {
-            return version switch
-            {
-                GuidVersion.Empty => EmptyGuidGenerator.Instance,
-                GuidVersion.Version1 => TimeBasedGuidGenerator.Instance,
-                GuidVersion.Version2 => DceSecurityGuidGenerator.Instance,
-                GuidVersion.Version3 => NameBasedGuidGenerator.MD5Hashing.Instance,
-                GuidVersion.Version4 => RandomizedGuidGenerator.Instance,
-                GuidVersion.Version5 => NameBasedGuidGenerator.SHA1Hashing.Instance,
-                _ => throw new ArgumentOutOfRangeException(nameof(version))
-            };
-        }
-
-        /// <summary>
-        /// 生成一个指定版本的 <see cref="Guid"/> 实例。
-        /// </summary>
-        /// <param name="version">要生成的 <see cref="Guid"/> 的版本。</param>
-        /// <returns>一个版本为 <paramref name="version"/> 的 <see cref="Guid"/> 实例。</returns>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// <paramref name="version"/> 不为有效的 <see cref="GuidVersion"/> 枚举值。</exception>
-        public static Guid NewGuid(GuidVersion version)
-        {
-            return GuidGenerator.OfVersion(version).NewGuid();
-        }
-
-        /// <summary>
-        /// 根据命名空间和名称生成一个指定版本的 <see cref="Guid"/> 实例。
-        /// </summary>
-        /// <param name="version">要生成的 <see cref="Guid"/> 的版本。</param>
-        /// <param name="ns">生成 <see cref="Guid"/> 时使用的命名空间。</param>
-        /// <param name="name">生成 <see cref="Guid"/> 时使用的名称。</param>
-        /// <returns>根据 <paramref name="ns"/> 和 <paramref name="name"/>
-        /// 生成的版本为 <paramref name="version"/> 的 <see cref="Guid"/> 实例。</returns>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// <paramref name="version"/> 不为有效的 <see cref="GuidVersion"/> 枚举值。</exception>
-        /// <exception cref="ArgumentNullException">
-        /// <paramref name="name"/> 为 <see langword="null"/>。</exception>
-        public static Guid NewGuid(GuidVersion version, Guid ns, string name)
-        {
-            return GuidGenerator.OfVersion(version).NewGuid(ns, name);
-        }
 
         /// <summary>
         /// 获取当前实例生成的 <see cref="Guid"/> 的版本。
@@ -74,6 +24,24 @@ namespace XstarS.GuidGenerators
         /// <returns>若当前实例生成的 <see cref="Guid"/> 依赖于输入参数，
         /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
         public virtual bool RequiresInput => this.Version.IsNameBased();
+
+        /// <summary>
+        /// 获取指定 GUID 版本的 <see cref="GuidGenerator"/> 对象。
+        /// </summary>
+        /// <param name="version">要生成的 <see cref="Guid"/> 的版本。</param>
+        /// <returns>版本为 <paramref name="version"/> 的 <see cref="GuidGenerator"/>。</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="version"/> 不为有效的 <see cref="GuidVersion"/> 枚举值。</exception>
+        public static GuidGenerator OfVersion(GuidVersion version) => version switch
+        {
+            GuidVersion.Empty => EmptyGuidGenerator.Instance,
+            GuidVersion.Version1 => TimeBasedGuidGenerator.Instance,
+            GuidVersion.Version2 => DceSecurityGuidGenerator.Instance,
+            GuidVersion.Version3 => NameBasedGuidGenerator.MD5Hashing.Instance,
+            GuidVersion.Version4 => RandomizedGuidGenerator.Instance,
+            GuidVersion.Version5 => NameBasedGuidGenerator.SHA1Hashing.Instance,
+            _ => throw new ArgumentOutOfRangeException(nameof(version))
+        };
 
         /// <summary>
         /// 生成一个新的 <see cref="Guid"/> 实例。
