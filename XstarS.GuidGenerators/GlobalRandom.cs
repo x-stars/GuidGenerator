0291cdb72180f83e7d34e8524830ce5430a314f3
天南十字星 <zjh96@live.cn>
2022-06-11 00:42:59 +0800

    Add `GlobalRandom` type to provide random numbers.

---
+++ XstarS.GuidGenerators/GlobalRandom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace XstarS.GuidGenerators
+{
+    internal static class GlobalRandom
+    {
+        private static readonly Random Numbers = new Random();
+
+        public static int Next()
+        {
+            return GlobalRandom.Numbers.Next();
+        }
+
+        public static double NextDouble()
+        {
+            return GlobalRandom.Numbers.NextDouble();
+        }
+
+        public static void NextBytes(byte[] buffer)
+        {
+            GlobalRandom.Numbers.NextBytes(buffer);
+        }
+    }
+}
