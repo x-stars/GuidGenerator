37245e545fe03a9acf4e74902abf5dbd8bf3a78b
天南十字星 <zjh96@live.cn>
2022-06-12 09:41:30 +0800

    Inline GUID field extension methods aggressively.

--- XstarS.GuidGenerators/GuidRfc4122Fields.cs
+++ XstarS.GuidGenerators/GuidRfc4122Fields.cs
@@ -5,22 +5,29 @@ namespace XstarS.GuidGenerators
 {
     internal static unsafe class GuidRfc4122Fields
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static ref uint TimeLow(this ref Guid guid) =>
             ref *(uint*)guid.RefFieldAt(0);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static ref ushort TimeMid(this ref Guid guid) =>
             ref *(ushort*)guid.RefFieldAt(4);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static ref ushort TimeHi_Ver(this ref Guid guid) =>
             ref *(ushort*)guid.RefFieldAt(6);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static ref byte ClkSeqHi_Var(this ref Guid guid) =>
             ref *(byte*)guid.RefFieldAt(8);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static ref byte ClkSeqLow(this ref Guid guid) =>
             ref *(byte*)guid.RefFieldAt(9);
 
-        internal static byte* NodeId(this ref Guid guid) => (byte*)guid.RefFieldAt(10);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static byte* NodeId(this ref Guid guid) =>
+            (byte*)guid.RefFieldAt(10);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void SetNodeId(this ref Guid guid, byte[] nodeId)
