ba55bd43f480ef53c61a5f8c8242f0b62e39b2dd
天南十字星 <zjh96@live.cn>
2022-07-07 21:30:06 +0800

    Simplify type access in F# GUID module.

--- XstarS.GuidModule/GuidModule.fs
+++ XstarS.GuidModule/GuidModule.fs
@@ -33,77 +33,77 @@ module Guid =
 
     type Namespace = GuidNamespaces
 
+    type Generator = GuidGenerator
+
     [<CompiledName("Empty")>]
-    let empty = Guid.Empty
+    let empty: Guid = Guid.Empty
 
     [<CompiledName("NamespaceDns")>]
-    let nsDns = GuidNamespaces.Dns
+    let nsDns: Guid = Namespace.Dns
 
     [<CompiledName("NamespaceUrl")>]
-    let nsUrl = GuidNamespaces.Url
+    let nsUrl: Guid = Namespace.Url
 
     [<CompiledName("NamespaceOid")>]
-    let nsOid = GuidNamespaces.Oid
+    let nsOid: Guid = Namespace.Oid
 
     [<CompiledName("NamespaceX500")>]
-    let nsX500 = GuidNamespaces.X500
+    let nsX500: Guid = Namespace.X500
 
     [<CompiledName("DomainPerson")>]
-    let domainPerson = DceSecurityDomain.Person
+    let domainPerson: Domain = Domain.Person
 
     [<CompiledName("DomainGroup")>]
-    let domainGroup = DceSecurityDomain.Group
+    let domainGroup: Domain = Domain.Group
 
     [<CompiledName("DomainOrg")>]
-    let domainOrg = DceSecurityDomain.Org
+    let domainOrg: Domain = Domain.Org
 
     [<CompiledName("ToValueOption")>]
-    let inline private toVOption (result, output) =
-        if result then ValueSome output else ValueNone
+    let inline private toVOption (res, out) =
+        if res then ValueSome out else ValueNone
 
     [<CompiledName("ToValueOption2")>]
-    let inline private toVOption2 (result, output1, output2) =
-        if result then ValueSome struct (output1, output2) else ValueNone
+    let inline private toVOption2 (res, out1, out2) =
+        if res then ValueSome struct (out1, out2) else ValueNone
 
     [<CompiledName("NewVersion1")>]
-    let newV1 () =
-        GuidGenerator.Version1.NewGuid()
+    let newV1 () = Generator.Version1.NewGuid()
 
     [<CompiledName("NewVersion2")>]
-    let newV2 domain =
-        GuidGenerator.Version2.NewGuid(domain)
+    let newV2 (domain: Domain) =
+        Generator.Version2.NewGuid(domain)
 
     [<CompiledName("NewVersion2OfOrg")>]
-    let newV2Org localId =
-        GuidGenerator.Version2.NewGuid(domainOrg, Nullable localId)
+    let newV2Org (localId: int) =
+        Generator.Version2.NewGuid(domainOrg, localId)
 
     [<CompiledName("NewVersion3")>]
-    let newV3 nsId name =
-        GuidGenerator.Version3.NewGuid(nsId, name)
+    let newV3 (nsId: Guid) (name: byte[]) =
+        Generator.Version3.NewGuid(nsId, name)
 
     [<CompiledName("NewVersion3ByString")>]
-    let newV3S nsId name =
-        GuidGenerator.Version3.NewGuid(nsId, name, null)
+    let newV3S (nsId: Guid) (name: string) =
+        Generator.Version3.NewGuid(nsId, name)
 
     [<CompiledName("NewVersion3ByEncoding")>]
-    let newV3Enc nsId encoding name =
-        GuidGenerator.Version3.NewGuid(nsId, name, encoding)
+    let newV3Enc (nsId: Guid) (enc: Text.Encoding) (name: string) =
+        Generator.Version3.NewGuid(nsId, name, enc)
 
     [<CompiledName("NewVersion4")>]
-    let newV4 () =
-        GuidGenerator.Version4.NewGuid()
+    let newV4 () = Generator.Version4.NewGuid()
 
     [<CompiledName("NewVersion5")>]
-    let newV5 nsId name =
-        GuidGenerator.Version5.NewGuid(nsId, name)
+    let newV5 (nsId: Guid) (name: byte[]) =
+        Generator.Version5.NewGuid(nsId, name)
 
     [<CompiledName("NewVersion5ByString")>]
-    let newV5S nsId name =
-        GuidGenerator.Version5.NewGuid(nsId, name, null)
+    let newV5S (nsId: Guid) (name: string) =
+        Generator.Version5.NewGuid(nsId, name)
 
     [<CompiledName("NewVersion5ByEncoding")>]
-    let newV5Enc nsId encoding name =
-        GuidGenerator.Version5.NewGuid(nsId, name, encoding)
+    let newV5Enc (nsId: Guid) (enc: Text.Encoding) (name: string) =
+        Generator.Version5.NewGuid(nsId, name, enc)
 
     [<CompiledName("OfFields")>]
     let ofFields (a: int) b c d e f g h i j k =
@@ -157,10 +157,10 @@ module Guid =
         guid.ToString(format)
 
     [<CompiledName("GetVariant")>]
-    let variant (guid: Guid) = guid.GetVariant()
+    let variant (guid: Guid) : Variant = guid.GetVariant()
 
     [<CompiledName("GetVersion")>]
-    let version (guid: Guid) = guid.GetVersion()
+    let version (guid: Guid) : Version = guid.GetVersion()
 
     [<CompiledName("IsRfc4122Uuid")>]
     let internal isRfc4122 (guid: Guid) =
@@ -187,17 +187,17 @@ module Guid =
         (isRfc4122 guid) && (version guid).ContainsNodeId()
 
     [<CompiledName("TryGetTimestamp")>]
-    let tryGetTimestamp (guid: Guid) =
+    let tryGetTime (guid: Guid) : DateTime voption =
         guid.TryGetTimestamp() |> toVOption
 
     [<CompiledName("TryGetClockSequence")>]
-    let tryGetClockSeq (guid: Guid) =
+    let tryGetClockSeq (guid: Guid) : int16 voption =
         guid.TryGetClockSequence() |> toVOption
 
     [<CompiledName("TryGetDomainAndLocalId")>]
-    let tryGetLocalId (guid: Guid) =
+    let tryGetLocalId (guid: Guid) : struct (Domain * int) voption =
         guid.TryGetDomainAndLocalId() |> toVOption2
 
     [<CompiledName("TryGetNodeId")>]
-    let tryGetNodeId (guid: Guid) =
+    let tryGetNodeId (guid: Guid) : byte[] voption =
         guid.TryGetNodeId() |> toVOption
