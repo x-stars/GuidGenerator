38027411d45f6a58d23635412047801c0ed63f73
天南十字星 <zjh96@live.cn>
2022-07-24 00:51:54 +0800

    Add F# operator to function translation function.

--- XstarS.GuidModule.UnitTests/Properties/AssertModule.MSTest.fs
+++ XstarS.GuidModule.UnitTests/Properties/AssertModule.MSTest.fs
@@ -11,15 +11,25 @@ namespace XNetEx.UnitTesting
 module internal UnitTestOperators =
 
     /// <summary>
-    /// Apply an action to a value and returns the value,
-    /// the value being on the left, the action on the right.
+    /// Build an enum value from an underlying value.
     /// </summary>
     /// <param name="value">The input value.</param>
-    /// <param name="action">The action to apply.</param>
-    /// <returns>The input value.</returns>
-    let inline ( |- ) value (action: 'T -> unit) =
-        action value
-        value
+    /// <returns>The value as an enumeration.</returns>
+    [<CompiledName("EnumOfValue")>]
+    let inline enumof<'T, 'Enum when 'Enum: enum<'T>> value =
+        LanguagePrimitives.EnumOfValue<'T, 'Enum> value
+
+    /// <summary>
+    /// Reverses the order of arguments of an infix operator
+    /// to translate it into a normal function.
+    /// </summary>
+    /// <param name="func">The infix operator function.</param>
+    /// <param name="arg2">The right operand of the operator.</param>
+    /// <param name="arg1">The left operand of the operator.</param>
+    /// <returns>The return value of the infix operator.</returns>
+    [<CompiledName("InvokeOpAsFunc")>]
+    let inline op (func: 'T1 -> 'T2 -> 'TR) =
+        fun arg2 arg1 -> func arg1 arg2
 
     /// <summary>
     /// Apply an action to a value and returns the value.
@@ -33,13 +43,15 @@ module internal UnitTestOperators =
         value
 
     /// <summary>
-    /// Build an enum value from an underlying value.
+    /// Apply an action to a value and returns the value,
+    /// the value being on the left, the action on the right.
     /// </summary>
     /// <param name="value">The input value.</param>
-    /// <returns>The value as an enumeration.</returns>
-    [<CompiledName("EnumOfValue")>]
-    let inline enumof<'T, 'Enum when 'Enum: enum<'T>> value =
-        LanguagePrimitives.EnumOfValue<'T, 'Enum> value
+    /// <param name="action">The action to apply.</param>
+    /// <returns>The input value.</returns>
+    let inline ( |- ) value (action: 'T -> unit) =
+        action value
+        value
 
 namespace XNetEx.UnitTesting.MSTest
 
