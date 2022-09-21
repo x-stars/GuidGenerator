ec55d71aa5e2422e470e54d69ced9d61116bee1c
天南十字星 <zjh96@live.cn>
2022-09-21 21:06:23 +0800

    Optimize pointer conversion for memory safety.

--- XstarS.GuidGenerators/GuidRfc4122Fields.cs
+++ XstarS.GuidGenerators/GuidRfc4122Fields.cs
@@ -3,34 +3,43 @@ using System.Runtime.CompilerServices;
 
 namespace XNetEx.Guids;
 
-internal static unsafe class GuidRfc4122Fields
+internal static class GuidRfc4122Fields
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static ref uint TimeLow(this ref Guid guid) =>
-        ref *(uint*)guid.RefFieldAt(0);
+        ref guid.RefFieldAt<uint>(0);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static ref ushort TimeMid(this ref Guid guid) =>
-        ref *(ushort*)guid.RefFieldAt(4);
+        ref guid.RefFieldAt<ushort>(4);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static ref ushort TimeHi_Ver(this ref Guid guid) =>
-        ref *(ushort*)guid.RefFieldAt(6);
+        ref guid.RefFieldAt<ushort>(6);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static ref byte ClkSeqHi_Var(this ref Guid guid) =>
-        ref *(byte*)guid.RefFieldAt(8);
+        ref guid.RefFieldAt<byte>(8);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static ref byte ClkSeqLow(this ref Guid guid) =>
-        ref *(byte*)guid.RefFieldAt(9);
+        ref guid.RefFieldAt<byte>(9);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    internal static byte* NodeId(this ref Guid guid) =>
-        (byte*)guid.RefFieldAt(10);
+    internal static ref byte NodeId(this ref Guid guid, int index) =>
+        ref guid.RefFieldAt<byte>(10 + index);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    internal static void SetNodeId(this ref Guid guid, byte[] nodeId)
+    internal static unsafe byte* NodeId(this ref Guid guid)
+    {
+        fixed (Guid* pGuid = &guid)
+        {
+            return (byte*)pGuid + 10;
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static unsafe void SetNodeId(this ref Guid guid, byte[] nodeId)
     {
         var size = Math.Min(nodeId.Length, 6);
         fixed (byte* pNodeId = &nodeId[0])
@@ -40,11 +49,12 @@ internal static unsafe class GuidRfc4122Fields
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static void* RefFieldAt(this ref Guid guid, int offset)
+    private static unsafe ref T RefFieldAt<T>(this ref Guid guid, int offset)
+        where T : unmanaged
     {
         fixed (Guid* pGuid = &guid)
         {
-            return (byte*)pGuid + offset;
+            return ref *(T*)((byte*)pGuid + offset);
         }
     }
 }
