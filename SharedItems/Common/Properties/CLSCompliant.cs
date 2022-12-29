08c42ae358d054b236dc6be238c7633dfbf5930d
天南十字星 <zjh96@live.cn>
2022-12-29 21:18:06 +0800

    Remove `Attribute` suffix of attribute usage.

--- SharedItems/Common/Properties/CLSCompliant.cs
+++ SharedItems/Common/Properties/CLSCompliant.cs
@@ -2,8 +2,6 @@
 // This file is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-#pragma warning disable // IDE0001: Simplify name
-
 // Indicates whether this assembly is compliant
 // with the Common Language Specification (CLS).
-[assembly: System.CLSCompliantAttribute(true)]
+[assembly: System.CLSCompliant(true)]
--- SharedItems/SkipLocalsInit/Properties/SkipLocalsInit.cs
+++ SharedItems/SkipLocalsInit/Properties/SkipLocalsInit.cs
@@ -2,8 +2,6 @@
 // This file is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-#pragma warning disable // IDE0001: Simplify name
-
 // Indicates to the compiler that the .locals init flag
 // should not be set in nested method headers when emitting to metadata.
-[module: System.Runtime.CompilerServices.SkipLocalsInitAttribute]
+[module: System.Runtime.CompilerServices.SkipLocalsInit]
