7712913e7935359f8c85343d41416414147ae3d1
天南十字星 <zjh96@live.cn>
2023-02-19 11:22:02 +0800

    Add GUID generator native library demo project.

---
+++ XstarS.GuidGen.NativeLib/NativeLib.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using XNetEx.Guids;
+using XNetEx.Guids.Generators;
+using HResult = System.Int32;
+
+namespace XstarS.GuidGen;
+
+internal static unsafe class NativeLib
+{
+    static NativeLib() { ConfigureStateStorage(); }
+
+    [UnmanagedCallersOnly(EntryPoint = nameof(GuidCreateV1))]
+    internal static HResult GuidCreateV1([Out] Guid* guid)
+    {
+        return GuidCreate(GuidGenerator.Version1, guid);
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = nameof(GuidCreateV1R))]
+    internal static HResult GuidCreateV1R([Out] Guid* guid)
+    {
+        return GuidCreate(GuidGenerator.Version1R, guid);
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = nameof(GuidCreateV2))]
+    internal static HResult GuidCreateV2([Out] Guid* guid, DceSecurityDomain domain)
+    {
+        return GuidCreateDceSecurity(GuidGenerator.Version2, guid, domain);
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = nameof(GuidCreateV2Org))]
+    internal static HResult GuidCreateV2Org([Out] Guid* guid, uint localId)
+    {
+        return GuidCreateDceSecurity(
+            GuidGenerator.Version2, guid, DceSecurityDomain.Org, localId);
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = nameof(GuidCreateV2Other))]
+    internal static HResult GuidCreateV2Other(
+        [Out] Guid* guid, DceSecurityDomain domain, uint localId)
+    {
+        return GuidCreateDceSecurity(GuidGenerator.Version2, guid, domain, localId);
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = nameof(GuidCreateV3))]
+    internal static HResult GuidCreateV3(
+        [Out] Guid* guid, [In] Guid* nsId, [In] byte* name, nuint nameLen)
+    {
+        return GuidCreateNameBased(GuidGenerator.Version3, guid, nsId, name, nameLen);
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = nameof(GuidCreateV4))]
+    internal static HResult GuidCreateV4([Out] Guid* guid)
+    {
+        return GuidCreate(GuidGenerator.Version4, guid);
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = nameof(GuidCreateV5))]
+    internal static HResult GuidCreateV5(
+        [Out] Guid* guid, [In] Guid* nsId, [In] byte* name, nuint nameLen)
+    {
+        return GuidCreateNameBased(GuidGenerator.Version5, guid, nsId, name, nameLen);
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = nameof(GuidCreateV6))]
+    internal static HResult GuidCreateV6([Out] Guid* guid)
+    {
+        return GuidCreate(GuidGenerator.Version6, guid);
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = nameof(GuidCreateV6P))]
+    internal static HResult GuidCreateV6P([Out] Guid* guid)
+    {
+        return GuidCreate(GuidGenerator.Version6P, guid);
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = nameof(GuidCreateV7))]
+    internal static HResult GuidCreateV7([Out] Guid* guid)
+    {
+        return GuidCreate(GuidGenerator.Version7, guid);
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = nameof(GuidCreateV8))]
+    internal static HResult GuidCreateV8([Out] Guid* guid)
+    {
+        return GuidCreate(GuidGenerator.Version8, guid);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static HResult GuidCreate(IGuidGenerator guidGen, Guid* guid)
+    {
+        try
+        {
+            *guid = guidGen.NewGuid();
+            return default(HResult);
+        }
+        catch (Exception ex)
+        {
+            Trace.WriteLine(ex);
+            return ex.HResult;
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static HResult GuidCreateNameBased(
+        INameBasedGuidGenerator guidGen,
+        Guid* guid, Guid* nsId, byte* name, nuint nameLen)
+    {
+        try
+        {
+            *guid = guidGen.NewGuid(*nsId,
+                new ReadOnlySpan<byte>(name, checked((int)nameLen)));
+            return default(HResult);
+        }
+        catch (Exception ex)
+        {
+            Trace.WriteLine(ex);
+            return ex.HResult;
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static HResult GuidCreateDceSecurity(
+        IDceSecurityGuidGenerator guidGen,
+        Guid* guid, DceSecurityDomain domain, uint? localId = null)
+    {
+        try
+        {
+            *guid = guidGen.NewGuid(domain,
+                Unsafe.As<uint?, int?>(ref localId));
+            return default(HResult);
+        }
+        catch (Exception ex)
+        {
+            Trace.WriteLine(ex);
+            return ex.HResult;
+        }
+    }
+
+    private static void ConfigureStateStorage()
+    {
+        GuidGenerator.StateStorageException += (sender, e) =>
+        {
+            if ((e.OperationType != FileAccess.Read) ||
+                (e.Exception is not FileNotFoundException))
+            {
+                Trace.WriteLine(e.Exception);
+            }
+        };
+        var storageDir = Environment.GetFolderPath(
+            Environment.SpecialFolder.LocalApplicationData,
+            Environment.SpecialFolderOption.Create);
+        var storageFile = "768a7b1b-ae51-5c0a-bc9d-a85a343f2c24.state.bin";
+        var storagePath = Path.Combine(storageDir, storageFile);
+        _ = GuidGenerator.SetStateStorageFile(storagePath);
+    }
+}
---
+++ XstarS.GuidGen.NativeLib/XstarS.GuidGen.NativeLib.h
@@ -0,0 +1,78 @@
+#ifndef __XSTARS_GUIDGEN_NATIVELIB_H__
+#define __XSTARS_GUIDGEN_NATIVELIB_H__
+
+#ifdef _WIN32
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <windows.h>
+#include <guiddef.h>
+
+#pragma comment(lib, "Xstars.GuidGen.NativeLib.lib")
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef UCHAR DCE_SECURITY_DOMAIN;
+const DCE_SECURITY_DOMAIN DCE_SECURITY_DOMAIN_PERSON = 0;
+const DCE_SECURITY_DOMAIN DCE_SECURITY_DOMAIN_GROUP = 1;
+const DCE_SECURITY_DOMAIN DCE_SECURITY_DOMAIN_ORG = 2;
+
+const GUID GUID_NAMESPACE_DNS = {
+    0x6ba7b810, 0x9dad, 0x11d1,
+    {0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8} };
+const GUID GUID_NAMESPACE_URL = {
+    0x6ba7b811, 0x9dad, 0x11d1,
+    {0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8} };
+const GUID GUID_NAMESPACE_OID = {
+    0x6ba7b812, 0x9dad, 0x11d1,
+    {0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8} };
+const GUID GUID_NAMESPACE_X500 = {
+    0x6ba7b814, 0x9dad, 0x11d1,
+    {0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8} };
+
+__declspec(dllimport)
+HRESULT GuidCreateV1(_Out_ GUID *Guid);
+__declspec(dllimport)
+HRESULT GuidCreateV1R(_Out_ GUID *Guid);
+
+__declspec(dllimport)
+HRESULT GuidCreateV2(_Out_ GUID *Guid, DCE_SECURITY_DOMAIN Domain);
+__declspec(dllimport)
+HRESULT GuidCreateV2Org(_Out_ GUID *Guid, UINT32 LocalId);
+__declspec(dllimport)
+HRESULT GuidCreateV2Other(_Out_ GUID *Guid,
+    DCE_SECURITY_DOMAIN Domain, UINT32 LocalId);
+
+__declspec(dllimport)
+HRESULT GuidCreateV3(_Out_ GUID *Guid,
+    _In_ const GUID *NsId, _In_ const UCHAR *Name, SIZE_T NameLen);
+
+__declspec(dllimport)
+HRESULT GuidCreateV4(_Out_ GUID *Guid);
+
+__declspec(dllimport)
+HRESULT GuidCreateV5(_Out_ GUID *Guid,
+    _In_ const GUID *NsId, _In_ const UCHAR* Name, SIZE_T NameLen);
+
+__declspec(dllimport)
+HRESULT GuidCreateV6(_Out_ GUID *Guid);
+__declspec(dllimport)
+HRESULT GuidCreateV6P(_Out_ GUID *Guid);
+
+__declspec(dllimport)
+HRESULT GuidCreateV7(_Out_ GUID *Guid);
+
+__declspec(dllimport)
+HRESULT GuidCreateV8(_Out_ GUID *Guid);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _WIN32 */
+
+#endif /* __XSTARS_GUIDGEN_NATIVELIB_H__ */
