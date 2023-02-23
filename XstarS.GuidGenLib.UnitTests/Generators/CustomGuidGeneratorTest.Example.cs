661ceabf8fcad583df68eb2865a0cc4fd34f4eaa
天南十字星 <zjh96@live.cn>
2023-02-24 00:24:26 +0800

    Add basic unit test cases for name-based V8.

--- XstarS.GuidGenLib.UnitTests/Generators/CustomGuidGeneratorTest.Example.cs
+++ XstarS.GuidGenLib.UnitTests/Generators/CustomGuidGeneratorTest.Example.cs
@@ -5,6 +5,56 @@ namespace XNetEx.Guids.Generators;
 
 partial class CustomGuidGeneratorTest
 {
+    private sealed class TestCustomGuidGenerator : CustomGuidGenerator
+    {
+        public TestCustomGuidGenerator(
+            DateTime epochDateTime = default,
+            NodeIdSource nodeIdSource = default)
+            : base(epochDateTime, nodeIdSource)
+        {
+        }
+
+        public override Guid NewGuid()
+        {
+            var guid = Guid.Empty;
+            this.FillVersionField(ref guid);
+            this.FillVariantField(ref guid);
+            return guid;
+        }
+
+        public new long GetCurrentTimestamp()
+        {
+            return base.GetCurrentTimestamp();
+        }
+
+        public new byte GetNodeIdByte(int index)
+        {
+            return base.GetNodeIdByte(index);
+        }
+
+        public new void GetNodeIdBytes(byte[] buffer)
+        {
+            base.GetNodeIdBytes(buffer);
+        }
+
+#if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        public new void GetNodeIdBytes(Span<byte> buffer)
+        {
+            base.GetNodeIdBytes(buffer);
+        }
+#endif
+
+        public new int GetRandomInt32()
+        {
+            return base.GetRandomInt32();
+        }
+
+        public new long GetRandomInt64()
+        {
+            return base.GetRandomInt64();
+        }
+    }
+
     [TestMethod]
     public void NewGuid_Version8Example_GetExpectedTimestamp()
     {
@@ -42,25 +92,5 @@ partial class CustomGuidGeneratorTest
         var guid1 = GuidGenerator.Version8.NewGuid();
         var randomBytes1 = GetGuidRandomBytes(guid1);
         CollectionAssert.AreNotEqual(randomBytes0, randomBytes1);
-    }
-
-    [TestMethod, Ignore]
-    public void NewGuid_Version8Example_GetSameNodeIdByte()
-    {
-        var guid0 = GuidGenerator.Version8.NewGuid();
-        var (_, _, _, _, _, _, _, _, _, nodeId0, _) = guid0;
-        var guid1 = GuidGenerator.Version8.NewGuid();
-        var (_, _, _, _, _, _, _, _, _, nodeId1, _) = guid1;
-        Assert.AreEqual(nodeId0, nodeId1);
-    }
-
-    [TestMethod, Ignore]
-    public void NewGuid_Version8Example_GetIncrementSequence()
-    {
-        var guid0 = GuidGenerator.Version8.NewGuid();
-        var (_, _, _, _, _, _, _, _, _, _, sequence0) = guid0;
-        var guid1 = GuidGenerator.Version8.NewGuid();
-        var (_, _, _, _, _, _, _, _, _, _, sequence1) = guid1;
-        Assert.AreEqual((byte)(sequence0 + 1), sequence1);
     }
 }
---
+++ XstarS.GuidGenLib.UnitTests/Generators/GuidGeneratorTest.NameBasedV8.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XNetEx.Guids.Generators;
+
+partial class GuidGeneratorTest
+{
+    [TestMethod]
+    public void NewGuid_Version8Sha256_GetExpectedGuid()
+    {
+        var nsId = GuidNamespaces.Dns;
+        var name = Array.Empty<byte>();
+        var guid = GuidGenerator.Version8Sha256.NewGuid(nsId, name);
+        var expected = Guid.Parse("0e38fb05-6337-8c50-a201-6e2ec68fd5ac");
+        Assert.AreEqual(expected, guid);
+    }
+
+    [TestMethod]
+    public void NewGuid_Version8Sha256StringName_GetExpectedGuid()
+    {
+        var nsId = GuidNamespaces.Dns;
+        var name = "www.example.com";
+        var guid = GuidGenerator.Version8Sha256.NewGuid(nsId, name);
+        var expected = Guid.Parse("401835fd-a627-870a-873f-ed73f2bc5b2c");
+        Assert.AreEqual(expected, guid);
+    }
+}
