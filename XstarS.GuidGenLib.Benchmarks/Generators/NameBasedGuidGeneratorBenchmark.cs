b2b3c697c53101304447fa7edd54fdef87b6042f
天南十字星 <zjh96@live.cn>
2023-04-26 21:09:23 +0800

    Add benchmark cases for name-based GUIDs.

---
+++ XstarS.GuidGenLib.Benchmarks/Generators/NameBasedGuidGeneratorBenchmark.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace XNetEx.Guids.Generators;
+
+[MemoryDiagnoser]
+public class NameBasedGuidGeneratorBenchmark
+{
+    [CLSCompliant(false)]
+    [Params(1, 10, 100, 1000)]
+    public int NameLength;
+
+    private byte[] Name = null!;
+
+    [GlobalSetup]
+    public void Initialize()
+    {
+        var length = this.NameLength;
+        var name = new byte[length];
+        new Random().NextBytes(name);
+        this.Name = name;
+    }
+
+    [Benchmark(Baseline = true)]
+    public void SumNameBytes()
+    {
+        var sum = 0;
+        var name = this.Name;
+        foreach (var @byte in name)
+        {
+            sum += @byte;
+        }
+    }
+
+    [Benchmark]
+    public void GuidV3Generate()
+    {
+        var guidGen = GuidGenerator.Version3;
+        var guid = guidGen.NewGuid(Guid.Empty, this.Name);
+    }
+
+    [Benchmark]
+    public void GuidV5Generate()
+    {
+        var guidGen = GuidGenerator.Version5;
+        var guid = guidGen.NewGuid(Guid.Empty, this.Name);
+    }
+
+    [Benchmark]
+    public void GuidV8NSha256Generate()
+    {
+        var guidGen = GuidGenerator.Version8NSha256;
+        var guid = guidGen.NewGuid(Guid.Empty, this.Name);
+    }
+
+    [Benchmark]
+    public void GuidV8NSha384Generate()
+    {
+        var guidGen = GuidGenerator.Version8NSha384;
+        var guid = guidGen.NewGuid(Guid.Empty, this.Name);
+    }
+
+    [Benchmark]
+    public void GuidV8NSha512Generate()
+    {
+        var guidGen = GuidGenerator.Version8NSha512;
+        var guid = guidGen.NewGuid(Guid.Empty, this.Name);
+    }
+}
