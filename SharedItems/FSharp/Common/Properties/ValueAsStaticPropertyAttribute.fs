7eb7bad7f29985f2e1b82476e60ed60b121233d4
天南十字星 <zjh96@live.cn>
2023-05-25 22:11:08 +0800

    Apply `ValueAsStaticProperty` to F# GUID constants.

---
+++ SharedItems/FSharp/Common/Properties/ValueAsStaticPropertyAttribute.fs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2023 XstarS
+// This file is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+namespace global
+
+open System
+
+/// <summary>
+/// This Attribute is used to make Value bindings like
+/// <code>let x = some code</code>
+/// operate like static properties.
+/// </summary>
+[<Sealed>]
+[<AttributeUsage(AttributeTargets.Property,
+                 AllowMultiple = false)>]
+type internal ValueAsStaticPropertyAttribute() =
+    inherit Attribute()
