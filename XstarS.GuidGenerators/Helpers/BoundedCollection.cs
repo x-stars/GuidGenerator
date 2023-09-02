f42f48a83c22a7b579f9a14e8b99f3eec64fbc70
天南十字星 <zjh96@live.cn>
2023-09-02 14:22:31 +0800

    Add `BoundedCollection` to avoid `WaitHandle`.

---
+++ XstarS.GuidGenerators/Helpers/BoundedCollection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+#if NETCOREAPP3_0_OR_GREATER
+using System.Diagnostics.CodeAnalysis;
+#endif
+
+namespace XNetEx.Collections.Concurrent;
+
+internal sealed class BoundedCollection<T> : IProducerConsumerCollection<T>
+{
+    private readonly ConcurrentQueue<T> Items;
+
+    private readonly int Capacity;
+
+    private volatile int ItemsCount;
+
+    public BoundedCollection(int capacity)
+    {
+        this.Items = new ConcurrentQueue<T>();
+        this.Capacity = (capacity >= 0) ? capacity :
+            throw new ArgumentOutOfRangeException(nameof(capacity));
+        this.ItemsCount = 0;
+    }
+
+    public int Count => this.Items.Count;
+
+    object ICollection.SyncRoot => ((ICollection)this.Items).SyncRoot;
+
+    bool ICollection.IsSynchronized => ((ICollection)this.Items).IsSynchronized;
+
+    public void CopyTo(T[] array, int index) => this.Items.CopyTo(array, index);
+
+    public IEnumerator<T> GetEnumerator() => this.Items.GetEnumerator();
+
+    public T[] ToArray() => this.Items.ToArray();
+
+    public bool TryAdd(T item)
+    {
+        if (Interlocked.Increment(ref this.ItemsCount) <= this.Capacity)
+        {
+            this.Items.Enqueue(item);
+            return true;
+        }
+        Interlocked.Decrement(ref this.ItemsCount);
+        return false;
+    }
+
+
+#if NETCOREAPP3_0_OR_GREATER
+    public bool TryTake([MaybeNullWhen(false)] out T item)
+#else
+    public bool TryTake(out T item)
+#endif
+    {
+        if (this.Items.TryDequeue(out item))
+        {
+            Interlocked.Decrement(ref this.ItemsCount);
+            return true;
+        }
+        return false;
+    }
+
+    void ICollection.CopyTo(Array array, int index)
+    {
+        ((ICollection)this.Items).CopyTo(array, index);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable)this.Items).GetEnumerator();
+    }
+}
