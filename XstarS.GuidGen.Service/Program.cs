dd13227db5801acbd8d3719d08788657aace92b2
天南十字星 <zjh96@live.cn>
2023-09-06 19:39:58 +0800

    Rename some internal symbols in web API demo.

--- XstarS.GuidGen.Service/Program.cs
+++ XstarS.GuidGen.Service/Program.cs
@@ -28,10 +28,10 @@ var storageDir = Environment.GetFolderPath(
 var storagePath = Path.Combine(storageDir, "768a7b1b-ae51-5c0a-bc9d-a85a343f2c24.state.bin");
 _ = GuidGenerator.SetStateStorageFile(storagePath);
 
-app.MapGet("/", (int? count) => HandleCount(GuidGenerator.Version4, count));
+app.MapGet("/", (int? count) => NewGuidCount(GuidGenerator.Version4, count));
 
-app.MapGet("/v1", (int? count) => HandleCount(GuidGenerator.Version1, count));
-app.MapGet("/v1r", (int? count) => HandleCount(GuidGenerator.Version1R, count));
+app.MapGet("/v1", (int? count) => NewGuidCount(GuidGenerator.Version1, count));
+app.MapGet("/v1r", (int? count) => NewGuidCount(GuidGenerator.Version1R, count));
 
 app.MapGet("/v2/person", () => GuidGenerator.Version2.NewGuid(DceSecurityDomain.Person));
 app.MapGet("/v2/group", () => GuidGenerator.Version2.NewGuid(DceSecurityDomain.Group));
@@ -45,7 +45,7 @@ app.MapGet("/v3/{ns}/{name}", (string ns, string name) =>
 app.MapPost("/v3/{ns}", (string ns, [FromBody] string name) =>
     GuidGenerator.Version3.NewGuid(ParseGuidNs(ns), ParseBase64(name)));
 
-app.MapGet("/v4", (int? count) => HandleCount(GuidGenerator.Version4, count));
+app.MapGet("/v4", (int? count) => NewGuidCount(GuidGenerator.Version4, count));
 
 app.MapGet("/v5/{ns}/{name}", (string ns, string name) =>
     GuidGenerator.Version5.NewGuid(ParseGuidNs(ns), name));
@@ -53,13 +53,13 @@ app.MapPost("/v5/{ns}", (string ns, [FromBody] string name) =>
     GuidGenerator.Version5.NewGuid(ParseGuidNs(ns), ParseBase64(name)));
 
 #if !FEATURE_DISABLE_UUIDREV
-app.MapGet("/v6", (int? count) => HandleCount(GuidGenerator.Version6, count));
-app.MapGet("/v6p", (int? count) => HandleCount(GuidGenerator.Version6P, count));
-app.MapGet("/v6r", (int? count) => HandleCount(GuidGenerator.Version6R, count));
+app.MapGet("/v6", (int? count) => NewGuidCount(GuidGenerator.Version6, count));
+app.MapGet("/v6p", (int? count) => NewGuidCount(GuidGenerator.Version6P, count));
+app.MapGet("/v6r", (int? count) => NewGuidCount(GuidGenerator.Version6R, count));
 
-app.MapGet("/v7", (int? count) => HandleCount(GuidGenerator.Version7, count));
+app.MapGet("/v7", (int? count) => NewGuidCount(GuidGenerator.Version7, count));
 
-app.MapGet("/v8", (int? count) => HandleCount(GuidGenerator.Version8, count));
+app.MapGet("/v8", (int? count) => NewGuidCount(GuidGenerator.Version8, count));
 app.MapGet("/v8n/{hash}/{ns}/{name}", (string hash, string ns, string name) =>
     ParseHashName(hash).NewGuid(ParseGuidNs(ns), name));
 app.MapPost("/v8n/{hash}/{ns}", (string hash, string ns, [FromBody] string name) =>
@@ -68,7 +68,7 @@ app.MapPost("/v8n/{hash}/{ns}", (string hash, string ns, [FromBody] string name)
 
 app.Run();
 
-static object HandleCount(IGuidGenerator guidGen, int? count) => (count is null) ?
+static object NewGuidCount(IGuidGenerator guidGen, int? count) => (count is null) ?
     (object)guidGen.NewGuid() : Enumerable.Range(0, (int)count).Select(index => guidGen.NewGuid());
 
 static Guid ParseGuidNs(string nsNameOrId) => nsNameOrId.ToUpperInvariant() switch
