f47a132ac25833b96cf0290f78bf00a386b9af14
天南十字星 <zjh96@live.cn>
2023-10-08 19:28:37 +0800

    Make GUID V7 global monotonic by default.

--- XstarS.GuidGen.NativeLib/XstarS.GuidGen.NativeLib.h
+++ XstarS.GuidGen.NativeLib/XstarS.GuidGen.NativeLib.h
@@ -63,8 +63,6 @@ HRESULT GuidCreateV6R(_Out_ GUID *Guid);
 
 __declspec(dllimport)
 HRESULT GuidCreateV7(_Out_ GUID *Guid);
-__declspec(dllimport)
-HRESULT GuidCreateV7M(_Out_ GUID *Guid);
 
 __declspec(dllimport)
 HRESULT GuidCreateV8(_Out_ GUID *Guid);
