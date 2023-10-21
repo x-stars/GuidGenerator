21696fef83803c41737d58ffc31255685288c9d7
天南十字星 <zjh96@live.cn>
2023-10-21 23:00:44 +0800

    Simplify F# unit test code for generator state.

--- XstarS.GuidModule.UnitTests/GuidGeneratorStateTest.fs
+++ XstarS.GuidModule.UnitTests/GuidGeneratorStateTest.fs
@@ -4,86 +4,80 @@ open System
 open System.Diagnostics
 open System.IO
 open Microsoft.VisualStudio.TestTools.UnitTesting
+open XNetEx.FSharp.Control
 open XNetEx.FSharp.UnitTesting.MSTest
 
 [<TestClass>]
 [<DoNotParallelize>]
 type GuidGeneratorStateTest() =
 
-    [<TestMethod>]
-    member _.LoadGeneratorState_FileWithRandomNodeId_GetNodeIdFromFile() =
-        let mutable exception': Exception = null
+    let catchStateExn () : ref<exn> =
+        let refExn = ref null
         Guid.onStateExn (fun e ->
             if e.OperationType = FileAccess.Read then
-                exception' <- e.Exception)
-        let fileName = Path.GetTempFileName()
-        try
-            let addByte1 = (op (+) 1) >> byte
-            if true then
-                use stream = new FileStream(fileName, FileMode.Create)
-                use writer = new BinaryWriter(stream)
-                writer.Write(4122)
-                writer.Write(0x08)
-                writer.Write(0L)
-                writer.Write(0)
-                writer.Write((Array.init 6 byte), 0, 6)
-                writer.Write((Array.init 6 addByte1), 0, 6)
-            fileName
-            |> Guid.loadState
-            |> Assert.true'
-            exception'
-            |> Assert.null'
-            Guid.newV1R ()
-            |> Guid.tryGetNodeId
-            |> tee (Assert.true' << ValueOption.isSome)
-            |> ValueOption.get
-            |> Assert.Seq.equalTo (Array.init 6 addByte1)
-        finally
-            if File.Exists(fileName) then
+                refExn.Value <- e.Exception)
+        refExn
+
+    let createTempFile (fileName: outref<string>): IDisposable =
+        let tempFile = Path.GetTempFileName()
+        fileName <- tempFile
+        new DisposeAction(fun _ ->
+            if File.Exists(tempFile) then
                 try
-                    File.Delete(fileName)
+                    File.Delete(tempFile)
                 with
-                | ex -> Debug.WriteLine(ex)
+                | ex -> Debug.WriteLine(ex))
+
+    [<TestMethod>]
+    member _.LoadGeneratorState_FileWithRandomNodeId_GetNodeIdFromFile() =
+        let refExn = catchStateExn ()
+        let mutable fileName = null
+        use tempFile = createTempFile &fileName
+        let addByte1 = (op (+) 1) >> byte
+        if true then
+            use stream = new FileStream(fileName, FileMode.Create)
+            use writer = new BinaryWriter(stream)
+            writer.Write(4122)
+            writer.Write(0x08)
+            writer.Write(0L)
+            writer.Write(0)
+            writer.Write((Array.init 6 byte), 0, 6)
+            writer.Write((Array.init 6 addByte1), 0, 6)
+        fileName
+        |> Guid.loadState
+        |> Assert.true'
+        refExn.Value
+        |> Assert.null'
+        Guid.newV1R ()
+        |> Guid.tryGetNodeId
+        |> tee (Assert.true' << ValueOption.isSome)
+        |> ValueOption.get
+        |> Assert.Seq.equalTo (Array.init 6 addByte1)
 
     [<TestMethod>]
     member _.OnStateException_NonExistingFile_CatchFileNotFoundException() =
-        let mutable exception': Exception = null
-        Guid.onStateExn (fun e ->
-            if e.OperationType = FileAccess.Read then
-                exception' <- e.Exception)
-        let fileName = Path.GetTempFileName()
-        try
-            fileName
-            |> tee (File.Delete)
-            |> Guid.loadState
-            |> Assert.false'
-            exception'
-            |> Assert.ofType<FileNotFoundException>
-        finally
-            if File.Exists(fileName) then
-                try
-                    File.Delete(fileName)
-                with
-                | ex -> Debug.WriteLine(ex)
+        let refExn = catchStateExn ()
+        let mutable fileName = null
+        use tempFile = createTempFile &fileName
+        fileName
+        |> tee (File.Delete)
+        |> Guid.loadState
+        |> Assert.false'
+        refExn.Value
+        |> Assert.ofType<FileNotFoundException>
 
     [<TestCleanup>]
     member _.ResetGeneratorState() =
-        let fileName = Path.GetTempFileName()
-        try
-            if true then
-                let emptyNodeId = Array.zeroCreate<byte> 6
-                use stream = new FileStream(fileName, FileMode.Create)
-                use writer = new BinaryWriter(stream)
-                writer.Write(4122)
-                writer.Write(0x00)
-                writer.Write(0L)
-                writer.Write(0)
-                writer.Write(emptyNodeId, 0, 6)
-                writer.Write(emptyNodeId, 0, 6)
-            Guid.loadState fileName |> ignore
-        finally
-            if File.Exists(fileName) then
-                try
-                    File.Delete(fileName)
-                with
-                | ex -> Debug.WriteLine(ex)
+        let mutable fileName = null
+        use tempFile = createTempFile &fileName
+        if true then
+            let emptyNodeId = Array.zeroCreate<byte> 6
+            use stream = new FileStream(fileName, FileMode.Create)
+            use writer = new BinaryWriter(stream)
+            writer.Write(4122)
+            writer.Write(0x00)
+            writer.Write(0L)
+            writer.Write(0)
+            writer.Write(emptyNodeId, 0, 6)
+            writer.Write(emptyNodeId, 0, 6)
+        Guid.loadState fileName |> ignore
