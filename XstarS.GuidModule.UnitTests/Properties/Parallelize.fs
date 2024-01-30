297a40873c9dd1c8150699fc8019c90ea8aa915e
天南十字星 <zjh96@live.cn>
2024-01-30 19:50:05 +0800

    Make unit tests run parallel in method scope.

---
+++ XstarS.GuidModule.UnitTests/Properties/Parallelize.fs
@@ -0,0 +1,7 @@
+﻿namespace global
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+// Indicates the parallelization level for unit tests run.
+[<assembly: Parallelize(Scope = ExecutionScope.MethodLevel)>]
+do ()
