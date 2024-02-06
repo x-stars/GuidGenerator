6980d78da39bd047fc9c0f5d6f530f0ad3401f91
天南十字星 <zjh96@live.cn>
2024-02-06 19:39:16 +0800

    Disable runtime marshalling in P/Invoke assemblies.

---
+++ SharedItems/NoRuntimeMarshal/Properties/DisableRuntimeMarshalling.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2024 XstarS
+// This file is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#if NET7_0_OR_GREATER
+// Disables the built-in runtime managed/unmanaged marshalling subsystem
+// for P/Invokes, Delegate types, and unmanaged function pointer invocations.
+[assembly: System.Runtime.CompilerServices.DisableRuntimeMarshalling]
+#endif
