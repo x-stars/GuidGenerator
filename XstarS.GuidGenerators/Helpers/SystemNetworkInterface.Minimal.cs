d6c0c1883984d4d2a403303c3a38dd239ce43fab
天南十字星 <zjh96@live.cn>
2024-02-29 21:10:53 +0800

    Add `#pragma warning disable` to minimal APIs.

--- XstarS.GuidGenerators/Helpers/SystemNetworkInterface.Minimal.cs
+++ XstarS.GuidGenerators/Helpers/SystemNetworkInterface.Minimal.cs
@@ -10,6 +10,9 @@
 // This file is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+#pragma warning disable
+#nullable enable
+
 using System;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
