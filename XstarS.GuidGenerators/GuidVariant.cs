2952c5c798b29bdc5db44c0d13767d6081e781da
天南十字星 <zjh96@live.cn>
2025-04-11 20:20:26 +0800

    Add RFC 9562 info in `GuidVariant` XML docs.

--- XstarS.GuidGenerators/GuidVariant.cs
+++ XstarS.GuidGenerators/GuidVariant.cs
@@ -11,9 +11,15 @@ public enum GuidVariant : byte
     /// Reserved for NCS UUID backward compatibility.
     /// </summary>
     Ncs = 0,
+#if !UUIDREV_DISABLE
+    /// <summary>
+    /// Represents the variant specified in RFC 4122 and RFC 9562.
+    /// </summary>
+#else
     /// <summary>
     /// Represents the variant specified in RFC 4122.
     /// </summary>
+#endif
     Rfc4122 = 1,
     /// <summary>
     /// Represents the variant of Microsoft legacy GUIDs.
