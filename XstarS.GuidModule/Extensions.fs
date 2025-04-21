ef122cb5cb791d7540cc7afde9b08388144ba626
天南十字星 <zjh96@live.cn>
2025-04-21 23:05:28 +0800

    Optimize F# custom hashing APIs by weak table.

--- XstarS.GuidModule/Extensions.fs
+++ XstarS.GuidModule/Extensions.fs
@@ -2,9 +2,12 @@
 
 open System
 open System.Runtime.CompilerServices
+#if !UUIDREV_DISABLE
+open System.Security.Cryptography
+#endif
 
 /// <summary>
-/// Provides extra extension methods for <see cref="GuidGenerator"/>.
+/// Provides extra extension methods for <see cref="T:XNetEx.Guids.Generators.GuidGenerator"/>.
 /// </summary>
 [<AutoOpen; Extension>]
 module internal GuidGeneratorExtensions =
@@ -27,3 +30,28 @@ module internal GuidGeneratorExtensions =
                 while true do
                     yield this.NewGuid()
             }
+
+#if !UUIDREV_DISABLE
+    type private WeakTableOfHashing = ConditionalWeakTable<HashAlgorithm, INameBasedGuidGenerator>
+
+    [<CompiledName("Version8NCache")>]
+    let private v8NCache = WeakTableOfHashing()
+
+    [<CompiledName("CreateVersion8NCallback")>]
+    let private createV8N = WeakTableOfHashing.CreateValueCallback(GuidGenerator.CreateVersion8N)
+
+    type GuidGenerator with
+
+        /// <summary>
+        /// Gets the cached <see cref="T:XNetEx.Guids.Generators.INameBasedGuidGenerator"/> instance
+        /// of the specified hash algorithm. If the hash algorithm does not exist in the cache, a new
+        /// <see cref="T:XNetEx.Guids.Generators.INameBasedGuidGenerator"/> instance will be created.
+        /// </summary>
+        /// <param name="hashing">The hash algorithm to get the cache.</param>
+        /// <returns>The cached <see cref="T:XNetEx.Guids.Generators.INameBasedGuidGenerator"/>
+        /// instance of <paramref name="hashing"/>.</returns>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="hashing"/> is null.</exception>
+        [<CompiledName("GetVersion8NCache")>]
+        static member GetVersion8NCache(hashing: HashAlgorithm) : INameBasedGuidGenerator =
+            v8NCache.GetValue(hashing, createV8N)
+#endif
