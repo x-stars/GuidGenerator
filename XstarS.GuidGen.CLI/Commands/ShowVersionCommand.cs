eeb6f0d29423c20a83067b0bedcaa4a38b55df03
天南十字星 <zjh96@live.cn>
2025-04-26 11:44:49 +0800

    Add version info command to CLI tool.

---
+++ XstarS.GuidGen.CLI/Commands/ShowVersionCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace XstarS.GuidGen.Commands;
+
+internal sealed class ShowVersionCommand : ProgramCommand
+{
+    internal static readonly ShowVersionCommand Instance = new();
+
+    private static readonly string[] VersionNames = ["-V", "-VERSION"];
+
+    private ShowVersionCommand() { }
+
+    public override bool TryExecute(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            return false;
+        }
+        var versionNames = ShowVersionCommand.VersionNames;
+        var versionArg = args[0].ToUpperInvariant();
+        if (Array.IndexOf(versionNames, versionArg) < 0)
+        {
+            return false;
+        }
+
+        var program = ThisAssembly.Info.Title;
+        var version = ThisAssembly.Info.InformationalVersion;
+        version = version[..version.LastIndexOf('+')];
+        Console.WriteLine($"{program}, {version}");
+        return true;
+    }
+}
