6fadf5eb605da66215aa8a238c3ca8df4b623ca4
天南十字星 <zjh96@live.cn>
2025-11-17 21:36:42 +0800

    Use core library new APIs in F# adapter APIs.

--- XstarS.GuidModule/Extensions.fs
+++ XstarS.GuidModule/Extensions.fs
@@ -2,9 +2,6 @@
 
 open System
 open System.Runtime.CompilerServices
-#if !UUIDREV_DISABLE
-open System.Security.Cryptography
-#endif
 
 /// <summary>
 /// Provides extra extension methods for <see cref="T:XNetEx.Guids.Generators.GuidGenerator"/>.
@@ -30,29 +27,3 @@ module internal GuidGeneratorExtraExtensions =
                 while true do
                     yield this.NewGuid()
             }
-
-#if !UUIDREV_DISABLE
-    type private WeakTableOfHashing = ConditionalWeakTable<HashAlgorithm, INameBasedGuidGenerator>
-
-    [<CompiledName("Version8NOfHashing")>]
-    let private v8NOfHashing = WeakTableOfHashing()
-
-    [<CompiledName("CreateVersion8NCallback")>]
-    let private createV8N = WeakTableOfHashing.CreateValueCallback(GuidGenerator.CreateVersion8N)
-
-    type GuidGenerator with
-
-        /// <summary>
-        /// Gets the related <see cref="T:XNetEx.Guids.Generators.INameBasedGuidGenerator"/>
-        /// instance of the specified hash algorithm.
-        /// </summary>
-        /// <param name="hashing">The hash algorithm to use.</param>
-        /// <returns>The related <see cref="T:XNetEx.Guids.Generators.INameBasedGuidGenerator"/>
-        /// instance of <paramref name="hashing"/>.</returns>
-        /// <exception cref="T:System.ArgumentNullException">
-        /// <paramref name="hashing"/> is null.</exception>
-        [<CompiledName("GetVersion8NOf")>]
-        static member GetVersion8NOf(hashing: HashAlgorithm) : INameBasedGuidGenerator =
-            if isNull hashing then nullArg (nameof hashing)
-            v8NOfHashing.GetValue(hashing, createV8N)
-#endif
